7fc72ff7748813e61a2f22ec50f01dc256002f48
Jesse-0x <63316022+Jesse-0x@users.noreply.github.com>
2025-06-03 20:59:59 -0700

    fix: finally fixing everything and now it's working with my color

--- astro.config.ts
+++ astro.config.ts
@@ -38,9 +38,10 @@ export default defineConfig({
       plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
       useDarkModeMediaQuery: false,
       themeCssSelector: (theme) => `[data-theme="${theme.name.split('-')[1]}"]`,
+      minSyntaxHighlightingColorContrast: 0,
       defaultProps: {
         wrap: true,
-        // collapseStyle: 'collapsible-auto',
+        collapseStyle: 'collapsible-auto',
         overridesByLang: {
           'ansi,bat,bash,batch,cmd,console,powershell,ps,ps1,psd1,psm1,sh,shell,shellscript,shellsession,text,zsh':
             {
@@ -48,33 +49,33 @@ export default defineConfig({
             },
         },
       },
-      // styleOverrides: {
-      //   codeFontSize: '0.75rem',
-      //   borderColor: 'var(--border)',
-      //   codeFontFamily: 'var(--font-mono)',
-      //   codeBackground:
-      //     'color-mix(in oklab, var(--secondary) 25%, transparent)',
-      //   frames: {
-      //     editorActiveTabForeground: 'var(--muted-foreground)',
-      //     editorActiveTabBackground:
-      //       'color-mix(in oklab, var(--secondary) 25%, transparent)',
-      //     editorActiveTabIndicatorBottomColor: 'transparent',
-      //     editorActiveTabIndicatorTopColor: 'transparent',
-      //     editorTabBorderRadius: '0',
-      //     editorTabBarBackground: 'transparent',
-      //     editorTabBarBorderBottomColor: 'transparent',
-      //     frameBoxShadowCssValue: 'none',
-      //     terminalBackground:
-      //       'color-mix(in oklab, var(--secondary) 25%, transparent)',
-      //     terminalTitlebarBackground: 'transparent',
-      //     terminalTitlebarBorderBottomColor: 'transparent',
-      //     terminalTitlebarForeground: 'var(--muted-foreground)',
-      //   },
-      //   lineNumbers: {
-      //     foreground: 'var(--muted-foreground)',
-      //   },
-      //   uiFontFamily: 'var(--font-sans)',
-      // },
+      styleOverrides: {
+        codeFontSize: '0.75rem',
+        borderColor: 'var(--border)',
+        codeFontFamily: 'var(--font-mono)',
+        codeBackground:
+          'color-mix(in oklab, var(--secondary) 25%, transparent)',
+        frames: {
+          editorActiveTabForeground: 'var(--muted-foreground)',
+          editorActiveTabBackground:
+            'color-mix(in oklab, var(--secondary) 25%, transparent)',
+          editorActiveTabIndicatorBottomColor: 'transparent',
+          editorActiveTabIndicatorTopColor: 'transparent',
+          editorTabBorderRadius: '0',
+          editorTabBarBackground: 'transparent',
+          editorTabBarBorderBottomColor: 'transparent',
+          frameBoxShadowCssValue: 'none',
+          terminalBackground:
+            'color-mix(in oklab, var(--secondary) 25%, transparent)',
+          terminalTitlebarBackground: 'transparent',
+          terminalTitlebarBorderBottomColor: 'transparent',
+          terminalTitlebarForeground: 'var(--muted-foreground)',
+        },
+        lineNumbers: {
+          foreground: 'var(--muted-foreground)',
+        },
+        uiFontFamily: 'var(--font-sans)',
+      },
     }),
     mdx(),
     react(),
